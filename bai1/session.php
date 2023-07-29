9ab07b5cc9587207c693698de290c267e19d42f9
ITPLUS Academy <thanghoang064@gmail.com>
2023-07-29 15:00:02 +0700

    update project

--- bai1/session.php
+++ bai1/session.php
@@ -2,6 +2,11 @@
 session_start();
 //$ten = "Nguyễn Văn A";
 //echo $ten;
-$_SESSION['ten'] = "nguyễn văn a";
-$_SESSION['tuoi'] = 18;
+// cách lưu biến session
+//$_SESSION['ten'] = "nguyễn văn a";
+//$_SESSION['tuoi'] = 18;
+//$_COOKIE['ten'] = "âbccc";
+//cách lưu trữ cookiee
+setcookie('ten','tài',time()-300); // nếu trừ thời gian đi thì giống như unset session
+setcookie('tuoi',18,time()+500);
 ?>
--- bai1/test.php
+++ bai1/test.php
@@ -1,5 +1,7 @@
 <?php
 //kích hoạt sử dụng session
-session_start();
-echo $_SESSION['ten'];
-echo  $_SESSION['tuoi'];
+//session_start();
+//echo $_SESSION['ten'];
+//echo  $_SESSION['tuoi'];
+echo "cookie".$_COOKIE['ten'];
+echo "cookie".$_COOKIE['tuoi'];
